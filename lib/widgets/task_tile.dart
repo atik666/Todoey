b623513c4b1f7ab3bc5a3efb97ce42f90f4fe6db
atik666 <faisal.atik@gmail.com>
2020-10-21 23:34:11 +0800

    commit 21/10/21

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -2,14 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:todoey_flutter/screens/tasks_screen.dart';
 
 class TaskTile extends StatelessWidget {
+
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkboxCallback;
+
+  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        'This is a task.',
+        taskTitle,
+        style: TextStyle(decoration: isChecked ?
+        TextDecoration.lineThrough :
+          TextDecoration.none,),
       ),
       trailing: Checkbox(
-        value: false,
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
       ),
     );
   }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/screens/tasks_screen.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
+import 'package:todoey_flutter/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Go to gym'),
+    Task(name: 'write papers'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(itemBuilder: (context, index) {
+      return TaskTile(taskTitle: tasks[index].name,
+        isChecked: tasks[index].isDone,
+        checkboxCallback: ( bool checkboxState){
+        setState(() {
+          tasks[index].toggleDone();
+        });
+      },
+      );
+    },
+      itemCount: tasks.length,
     );
   }
 }
